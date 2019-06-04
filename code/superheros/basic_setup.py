d55ad47fdef524e201f2db7e92cb403479e3b87d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-04 13:29:32 +0000

    Deploy arc-lessons/libraries-modules to github.com/arc-lessons/libraries-modules.git:gh-pages

---
+++ code/superheros/basic_setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(  name='superheros',
+        version=1.0,
+        py_modules=['superhero',
+                    'superhero2',
+                    'check_superheros'
+                    ]
+        )
---
+++ code/superheros/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(  name='superheros',
+        version=1.0,
+        author='Jack Betteridge',
+        author_email='jdb55@bath.ac.uk',
+        description='A package about superheros',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://arc-lessons.github.io/libraries-modules/00_schedule.html',
+        py_modules=['superhero',
+                    'superhero2',
+                    'check_superheros'
+                    ],
+        classifiers=[   'Programming Language :: Python :: 3',
+                        'License :: OSI Approved :: BSD License',
+                        'Operating System :: POSIX :: Linux'
+                    ]
+        )
