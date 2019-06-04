3b77ae2685f084882187f439dded67f2a69e5048
Jack Betteridge <J.D.Betteridge@bath.ac.uk>
2019-06-04 09:45:20 +0100

    Written outline of lesson, hopefully sufficient

--- code/morse/setup.py
+++ code/morse/setup.py
@@ -7,7 +7,7 @@ setup(  name='morse',
         description='Module for Morse code enthusiasts',
         long_description='A longer description of how Morese code works',
         url='https://arc-lessons.github.io/libraries-modules/07_package-soln.html',
-        py_modules=['morse']
+        py_modules=['morse'],
         classifiers=[   'Programming Language :: Python :: 3',
                         'License :: OSI Approved :: BSD License',
                         'Operating System :: POSIX :: Linux'
